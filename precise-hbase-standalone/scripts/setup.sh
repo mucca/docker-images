1047396e8af02f8285c8f261c5a3199d91a259a9
Saverio <saver.mucci@gmail.com>
2015-06-22 16:22:38 +0200

    avoid to run the rest server and update the hbase version

--- precise-hbase-standalone/scripts/setup.sh
+++ precise-hbase-standalone/scripts/setup.sh
@@ -7,8 +7,8 @@ mkdir -p /usr/local/hbase
 mkdir -p /var/log/hbase
 
 cd /
-wget http://mirror.tcpdiag.net/apache/hbase/stable/hbase-0.94.12.tar.gz
-tar -xvzf hbase-0.94.12.tar.gz
-rm hbase-0.94.12.tar.gz
-cp -r hbase-0.94.12/* /usr/local/hbase
-rm -rf hbase-0.94.12
+wget http://mirror.tcpdiag.net/apache/hbase/hbase-0.94.27/hbase-0.94.27.tar.gz
+tar -xvzf hbase-0.94.27.tar.gz
+rm hbase-0.94.27.tar.gz
+cp -r hbase-0.94.27/* /usr/local/hbase
+rm -rf hbase-0.94.27
